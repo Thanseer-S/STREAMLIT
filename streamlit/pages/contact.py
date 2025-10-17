4f7bfad22dd674d46d4e1795f9c2f640aa16c67c
Thanseer s <thanseerthanzi1234@gmail.com>
2025-10-17 16:18:59 +0530

    Add files via upload

---
+++ streamlit/pages/contact.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.write("this is my contact")
+import streamlit as st
+
+st.title("📞 Contact Me")
+
+st.write("Thank you for visiting! 😊")
+st.write("This is my contact section where you can get in touch with me.")
+st.write("Whether you have a question, a project idea, or just want to say hello, feel free to reach out.")
+st.write("I'm always open to new connections and collaborations!")
